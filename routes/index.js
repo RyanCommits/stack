0e30e71e676e29a58d02a125dcfae77a794c3583
Ryan Wang <qwang1993@gmail.com>
2017-09-08 11:11:38 -0400

    tweeked the front-endhome page and login/signin page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  res.locals.signupFeedback = req.flash('signupSuccess');
   res.render('index');
 });
 
