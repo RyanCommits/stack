76a53b08adf9b3f4cd437178b46d1ef21d830d24
Ryan Wang <qwang1993@gmail.com>
2017-09-09 15:27:31 -0400

    set up dashboard with profile page without functionality

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/dashboard', (req, res, next) => {
-  res.render('dash-views/dash.ejs', { layout: 'dashlayout.ejs' });
+router.get('/dashboard/', (req, res, next) => {
+  res.redirect('/dashboard/home');
+});
+
+router.get('/dashboard/home', (req, res, next) => {
+  res.locals.path = 'Dashboard';
+  res.render('dash-views/dashboard.ejs', { layout: 'dashlayout.ejs' });
+});
+
+router.get('/dashboard/profile', (req, res, next) => {
+  res.locals.path = 'Profile';
+  res.render('dash-views/profile.ejs', { layout: 'dashlayout.ejs' });
 });
 
 module.exports = router;
