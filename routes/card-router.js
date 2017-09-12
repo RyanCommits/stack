180408a88f7d374bb567285a29b20cbff8d3f4e4
Ryan Wang <qwang1993@gmail.com>
2017-09-12 19:26:17 -0400

    able to display all cards in stack

--- routes/card-router.js
+++ routes/card-router.js
@@ -20,6 +20,8 @@ router.post('/dashboard/home', (req, res, next) => {
 
 });
 
+// Delete stack --------------------------------------------------------------
+
 router.post('/dashboard/home/:stackId/delete', (req, res, next) => {
     StackModel.findByIdAndRemove(
       req.params.stackId,
@@ -33,6 +35,33 @@ router.post('/dashboard/home/:stackId/delete', (req, res, next) => {
           res.redirect('/dashboard/home');
       }
     );
+});
+
+// Edit stack ----------------------------------------------------------------
+
+router.get('/dashboard/:stackId', (req, res, next) => {
+
+  const currentStack = req.params.stackId;
+
+    StackModel.findById(
+      currentStack,
+
+      (err, oneStack) => {
+        // if there's a database error...
+        if (err) {
+            // skip to the error handler middleware
+            next(err);
+            // return to avoid showing the view
+            return;
+
+        }
+
+        // send the results to the view
+        res.locals.singleStack = oneStack;
+        res.locals.path = oneStack.stackName;
+
+        res.render('dash-views/edit-stack.ejs', { layout: 'dashlayout.ejs' });
+    });
 });
 
 module.exports = router;
