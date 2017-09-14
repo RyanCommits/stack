e28dadb655a16d474bb19245f183cfb254bd553d
Ryan Wang <qwang1993@gmail.com>
2017-09-14 13:04:56 -0400

    able to edit cards

--- public/javascripts/card.js
+++ public/javascripts/card.js
@@ -4,5 +4,15 @@ $(document).ready(function () {
   $('.edit-button').click(function () {
     const cardFront = $(this).parent().prev().prev().text();
     const cardBack = $(this).parent().prev().text();
+
+// extracts StackID and CardID
+    const cardEditAttr = $(this).parent().next().children().first().attr('action').split('/');
+
+    const idURL = cardEditAttr[2] + '/' + cardEditAttr[3];
+
+    $('.editFrontLabel').text(cardFront);
+    $('.editBackLabel').text(cardBack);
+    $('.editForm').attr("action", "/dashboard/" + idURL + "/edit");
+
   });
 });
