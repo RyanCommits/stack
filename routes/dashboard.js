ccdb2341871fde4d4598bc372e372d2c98118403
Ryan Wang <qwang1993@gmail.com>
2017-09-19 12:26:36 -0400

    set up basic dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,12 +8,47 @@ router.get('/dashboard/', (req, res, next) => {
 });
 
 router.get('/dashboard/home', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
-  res.render('dash-views/dashboard.ejs', { layout: 'dashlayout.ejs' });
+  // find all due stacks
+
+    StackModel.find({}, {cards: {$elemMatch: {dueToday: true}}},
+
+      (err, dueList) => {
+
+        if (err) {
+            next(err);
+            return;
+        }
+
+        res.locals.dueLength = dueList;
+
+
+  // find all Stacks
+
+    StackModel.find(
+
+      { user: req.user._id },
+
+      (err, stackList) => {
+
+        if (err) {
+            next(err);
+            return;
+        }
+
+        // send the results to the view
+        res.locals.allStacks = stackList;
+
+        res.locals.path = 'My Stacks';
+
+        res.render('dash-views/dashboard.ejs', { layout: 'dashlayout.ejs' });
+
+      });
+    });
 });
 
 router.get('/dashboard/my-stacks', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
 
-// find all due stacks\
+// find all due stacks
 
   StackModel.find({}, {cards: {$elemMatch: {dueToday: true}}},
 
@@ -56,6 +91,8 @@ router.get('/dashboard/profile', ensureLogin.ensureLoggedIn('/login'), (req, res
   res.render('dash-views/profile.ejs', { layout: 'dashlayout.ejs' });
 });
 
+// test all cards
+
 router.get('/dashboard/my-stacks/:stackId/test', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
 
   const currentStack = req.params.stackId;
@@ -77,6 +114,33 @@ router.get('/dashboard/my-stacks/:stackId/test', ensureLogin.ensureLoggedIn('/lo
     res.locals.path = oneStack.stackName;
 
     res.render('dash-views/test.ejs', { layout: 'dashlayout.ejs' });
+
+  });
+});
+
+// test only due cards
+
+router.get('/dashboard/my-stacks/:stackId/testdue', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
+
+  const currentStack = req.params.stackId;
+
+    StackModel.findById(
+      currentStack,
+
+      (err, oneStack) => {
+        // if there's a database error...
+        if (err) {
+            // skip to the error handler middleware
+            next(err);
+            // return to avoid showing the view
+            return;
+
+        }
+
+    res.locals.singleStack = oneStack;
+    res.locals.path = oneStack.stackName;
+
+    res.render('dash-views/testdue.ejs', { layout: 'dashlayout.ejs' });
 
   });
 });
